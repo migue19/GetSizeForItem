a5671d00b93a7c096bd03f57f0604f40ad86ec81
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2021-12-29 10:15:29 -0600

    Initial Commit

---
+++ GetSizeforItem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetSizeforItem
+//
+//  Created by Miguel Mexicano Herrera on 29/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
